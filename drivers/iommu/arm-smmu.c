53e7fba4304a67876b2b3ab97828f6ee753e7ddd
Patrick Daly <pdaly@codeaurora.org>
2017-02-13 22:14:53 -0800

    iommu: arm-smmu: Ensure add_device is called properly
    The add_device callback may be called during arm_smmu_device_dt_probe.
    In this case driver_find_device() skips  the device which is
    currently being probed. Restore the old list to fix this.
    
    Change-Id: I3ec70fd0f8d4a5eea9e2a8f8a8b91a435a79585f
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -128,6 +128,9 @@ struct arm_smmu_pte_info {
 
 static bool using_legacy_binding, using_generic_binding;
 
+static DEFINE_SPINLOCK(arm_smmu_devices_lock);
+static LIST_HEAD(arm_smmu_devices);
+
 struct arm_smmu_option_prop {
 	u32 opt;
 	const char *prop;
@@ -1881,6 +1884,22 @@ static int arm_smmu_init_asid(struct iommu_domain *domain,
 		cfg->asid = ret;
 	}
 	return 0;
+}
+
+static struct arm_smmu_device *arm_smmu_get_by_list(struct device_node *np)
+{
+	struct arm_smmu_device *smmu;
+	unsigned long flags;
+
+	spin_lock_irqsave(&arm_smmu_devices_lock, flags);
+	list_for_each_entry(smmu, &arm_smmu_devices, list) {
+		if (smmu->dev->of_node == np) {
+			spin_unlock_irqrestore(&arm_smmu_devices_lock, flags);
+			return smmu;
+		}
+	}
+	spin_unlock_irqrestore(&arm_smmu_devices_lock, flags);
+	return NULL;
 }
 
 static void arm_smmu_free_asid(struct iommu_domain *domain)
@@ -3748,7 +3767,7 @@ struct arm_smmu_device *arm_smmu_get_by_fwnode(struct fwnode_handle *fwnode)
 	struct device *dev = driver_find_device_by_fwnode(&arm_smmu_driver.driver,
 							  fwnode);
 	put_device(dev);
-	return dev ? dev_get_drvdata(dev) : NULL;
+	return dev ? dev_get_drvdata(dev) : arm_smmu_get_by_list(to_of_node(fwnode));
 }
 
 #ifdef CONFIG_MSM_TZ_SMMU
@@ -5500,6 +5519,11 @@ static int arm_smmu_device_dt_probe(struct platform_device *pdev)
 		pm_runtime_set_active(dev);
 		pm_runtime_enable(dev);
 	}
+
+	INIT_LIST_HEAD(&smmu->list);
+	spin_lock(&arm_smmu_devices_lock);
+	list_add(&smmu->list, &arm_smmu_devices);
+	spin_unlock(&arm_smmu_devices_lock);
 
 	/*
 	 * For ACPI and generic DT bindings, an SMMU will be probed before
